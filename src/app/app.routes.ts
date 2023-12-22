4eb720b3eb5ff728180ab55c85e38398c1081801
Daxi90 <hdateam90@gmail.com>
2023-12-22 14:11:03 +0100

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StartseiteComponent } from './startseite/startseite.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StartseiteComponent },
+];
