0abf11e4b3ab1cf4473dfc599c5a37888c4005c5
Daxi90 <hdateam90@gmail.com>
2023-12-22 14:54:18 +0100

    Finished Above the fold

--- src/app/startseite/startseite.component.ts
+++ src/app/startseite/startseite.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [],
+  imports: [AboveTheFoldComponent],
   templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.scss'
 })
