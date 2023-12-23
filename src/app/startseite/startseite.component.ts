d1f17dc03fbc2cd4d98773c03384c916bf4d135f
Daxi90 <hdateam90@gmail.com>
2023-12-23 11:00:13 +0100

    Started about me section

--- src/app/startseite/startseite.component.ts
+++ src/app/startseite/startseite.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { DividerSectionComponent } from './divider-section/divider-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [AboveTheFoldComponent, DividerSectionComponent],
+  imports: [AboveTheFoldComponent, DividerSectionComponent, AboutMeComponent],
   templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.scss'
 })
