e5bdffe9b463c15989696d497ab373ff17652a07
Daxi90 <hdateam90@gmail.com>
2023-12-23 10:04:38 +0100

    Finished Divider Section

--- src/app/startseite/startseite.component.ts
+++ src/app/startseite/startseite.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { DividerSectionComponent } from './divider-section/divider-section.component';
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [AboveTheFoldComponent],
+  imports: [AboveTheFoldComponent, DividerSectionComponent],
   templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.scss'
 })
