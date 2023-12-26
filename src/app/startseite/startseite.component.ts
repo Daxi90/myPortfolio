8c1aafd29d65760a7cc0088d65b29f3285bbca3f
Daxi90 <hdateam90@gmail.com>
2023-12-26 16:15:41 +0100

    Finished first divider arrow sectio

--- src/app/startseite/startseite.component.ts
+++ src/app/startseite/startseite.component.ts
@@ -3,11 +3,12 @@ import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component
 import { DividerSectionComponent } from './divider-section/divider-section.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { DividerSectionArrow1Component } from './divider-section-arrow-1/divider-section-arrow-1.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [AboveTheFoldComponent, DividerSectionComponent, AboutMeComponent, DividerSectionArrow1Component],
+  imports: [AboveTheFoldComponent, DividerSectionComponent, AboutMeComponent, DividerSectionArrow1Component, MySkillsComponent],
   templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.scss'
 })
