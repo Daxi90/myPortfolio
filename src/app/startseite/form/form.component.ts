36494507604e13b89ac18d02a3a24da104040cf2
Daxi90 <hdateam90@gmail.com>
2024-01-06 10:56:28 +0100

    started designin form

--- src/app/startseite/form/form.component.ts
+++ src/app/startseite/form/form.component.ts
@@ -11,13 +11,40 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class FormComponent {
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
 
-  sendMail() {
+  async sendMail() {
     console.log('Sending Mail ', this.myForm);
-    this.nameField.nativeElement.disabled = true;
-    this.messageField.nativeElement.disabled = true;
-    this.sendButton.nativeElement.disabled = true;
+
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+    //Animation anzeigen
+
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
+    fd.append('message', messageField.value);
+
+    // Senden
+    await fetch('https://www.test.at', {
+      method: 'POST',
+      body: fd
+    })
+
+    //Text anzeigen, Nachricht gesendet
+    nameField.disabled = false;
+    emailField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
+
   }
 }
