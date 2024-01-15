576155ccf382c34876a3b8b1ce36f9e4c6fcaa21
Daxi90 <hdateam90@gmail.com>
2024-01-15 13:12:01 +0100

    Added Burger animation

--- src/app/startseite/header/header.component.ts
+++ src/app/startseite/header/header.component.ts
@@ -13,6 +13,18 @@ export class HeaderComponent implements OnDestroy {
   menuOpen = false;
   subscription: Subscription;
 
+  currentImage = '/assets/img/burgerClosed.png';
+
+  menuImages = [
+    '/assets/img/burgerClosed.png',
+    '/assets/img/burgerClosed1.png',
+    '/assets/img/burgerClosed2.png',
+    '/assets/img/burgerClosed3.png',
+  ];
+
+  private imageIndex = 0;
+  private animationInProgress = false;
+
   constructor(private toggleMenuService: ToggleMenuService) {
     this.subscription = this.toggleMenuService.currentToggle.subscribe(toggle => {
       this.menuOpen = toggle;
@@ -21,9 +33,36 @@ export class HeaderComponent implements OnDestroy {
 
   toggleMenuFunc() {
     this.toggleMenuService.changeToggle(!this.menuOpen); // Toggelt den Zustand
+
+    if (!this.animationInProgress) {
+      this.animateMenu();
+    }
   }
+
+  private animateMenu() {
+    this.animationInProgress = true;
+    this.imageIndex = 0; // Starte die Animation vom ersten Bild
+  
+    const animationInterval = setInterval(() => {
+      this.currentImage = this.menuImages[this.imageIndex];
+      this.imageIndex++;
+  
+      if (this.imageIndex >= this.menuImages.length) {
+        clearInterval(animationInterval);
+  
+        // Warte eine halbe Sekunde, bevor das erste Bild wieder geladen wird
+        setTimeout(() => {
+          this.currentImage = this.menuImages[0]; // Setzt das Bild zurück auf das erste Bild
+          this.animationInProgress = false;
+          this.imageIndex = 0; // Setzt den Index zurück
+        }, 500); // 500ms Pause
+      }
+    }, 100); // 100ms für jedes Bild
+  }
+  
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+
 }
